d542f6c25e99e39ce94091c05c5f9d62147c833c
Bruno Brito <bhdebrito@gmail.com>
2021-05-09 03:57:29 -0300

    <git-autocommit>

--- src/awesome-github-stats/Models/Styles.cs
+++ src/awesome-github-stats/Models/Styles.cs
@@ -2,9 +2,9 @@
 {
     public class Styles
     {
-        public string TextColor { get; set; }
+        public string TextColor { get; set; } = "#333";
         public string TitleColor { get; set; }
-        public string IconColor { get; set; }
+        public string IconColor { get; set; } = "#4c71f2";
         public bool ShowIcons { get; set; }
     }
 }
