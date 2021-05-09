1d1c9c919012bf9603cc5ca7cae7dad77be961e1
Bruno Brito <bhdebrito@gmail.com>
2021-05-09 02:29:37 -0300

    <git-autocommit>

--- tests/Tests/IntegrationTests/GithubServiceTests.cs
+++ tests/Tests/IntegrationTests/GithubServiceTests.cs
@@ -1,6 +1,7 @@
 using AwesomeGithubStats.Core.Interfaces;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Tests.Configuration;
 using Xunit;
@@ -10,10 +11,12 @@ namespace Tests.IntegrationTests
 {
     public class GithubServiceTests : IClassFixture<Warmup>
     {
+        private readonly ITestOutputHelper _output;
         private readonly IGithubService _githubService;
 
         public GithubServiceTests(Warmup services, ITestOutputHelper output)
         {
+            _output = output;
             _githubService = services.Services.GetRequiredService<IGithubService>();
         }
 
@@ -28,6 +31,7 @@ namespace Tests.IntegrationTests
         {
             var stats = await _githubService.GetUserStats(username);
 
+            _output.WriteLine($"Stats: {JsonSerializer.Serialize(stats, new JsonSerializerOptions() { WriteIndented = true })}");
             stats.Should().NotBeNull();
             stats.Commits.Should().BeGreaterThan(0);
         }
