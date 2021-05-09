5fa89ce3dd5bcb291c105fab6101a181ebec38b8
Bruno Brito <bhdebrito@gmail.com>
2021-05-09 02:12:24 -0300

    <git-autocommit>

--- src/AwesomeGithubStats.Core/Models/UserRank.cs
+++ src/AwesomeGithubStats.Core/Models/UserRank.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace AwesomeGithubStats.Core.Models
+﻿namespace AwesomeGithubStats.Core.Models
 {
     public class UserRank
     {
@@ -27,25 +25,5 @@ namespace AwesomeGithubStats.Core.Models
 
         public string Level { get; set; }
         public double Score { get; set; }
-
-
-        private double Normalcdf(double mean, double sigma, double to)
-        {
-            var z = (to - mean) / Math.Sqrt(2 * sigma * sigma);
-            var t = 1 / (1 + 0.3275911 * Math.Abs(z));
-            var a1 = 0.254829592;
-            var a2 = -0.284496736;
-            var a3 = 1.421413741;
-            var a4 = -1.453152027;
-            var a5 = 1.061405429;
-            var erf =
-                1 - ((((a5 * t + a4) * t + a3) * t + a2) * t + a1) * t * Math.Exp(-z * z);
-            var sign = 1;
-            if (z < 0)
-            {
-                sign = -1;
-            }
-            return (1.0 / 2.0) * (1 + sign * erf);
-        }
     }
 }
