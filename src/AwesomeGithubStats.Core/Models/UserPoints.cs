a076b10bc18122d05a9e4518825dfab7628ba46e
Bruno Brito <bhdebrito@gmail.com>
2021-05-09 00:45:31 -0300

    <git-autocommit>

--- src/AwesomeGithubStats.Core/Models/UserPoints.cs
+++ src/AwesomeGithubStats.Core/Models/UserPoints.cs
@@ -4,10 +4,10 @@
     {
         public double PullRequests { get; set; } = 0.2;
         public double Commits { get; set; } = 0.5;
-        public double CommitsToMyRepositories { get; set; } = 0.5;
+        public double CommitsToMyRepositories { get; set; } = 1;
         public double CommitsToAnotherRepositories { get; set; } = 2;
-        public double PullRequestsToAnotherRepositories { get; set; } = 1.2;
-        public double Issues { get; set; } = 1;
+        public double PullRequestsToAnotherRepositories { get; set; } = 5;
+        public double Issues { get; set; } = 3;
         public double CreatedRepositories { get; set; } = 0.3;
         public double DirectStars { get; set; } = 1.0;
         public double IndirectStars { get; set; } = 0.55;
