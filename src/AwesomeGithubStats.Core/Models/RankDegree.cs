e5d607fa9121bfdde7e12bbb5b5c298d8724503e
Bruno Brito <bhdebrito@gmail.com>
2021-05-09 01:49:06 -0300

    <git-autocommit>

--- src/AwesomeGithubStats.Core/Models/RankDegree.cs
+++ src/AwesomeGithubStats.Core/Models/RankDegree.cs
@@ -5,6 +5,9 @@ namespace AwesomeGithubStats.Core.Models
 {
     public class RankDegree : Dictionary<string, int>
     {
+        /// <summary>
+        /// This score is based in user sindresorhus. An astonishing developer with many, many contributions.
+        /// </summary>
         public double MaxPoints { get; set; } = 7000;
 
         public KeyValuePair<string, int> InRange(double value)
