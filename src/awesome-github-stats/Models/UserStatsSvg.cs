586f9154ac5a6c2e6c1c735db625998decc6dd1a
Bruno Brito <bhdebrito@gmail.com>
2021-05-09 04:19:43 -0300

    <git-autocommit>

--- src/awesome-github-stats/Models/UserStatsSvg.cs
+++ src/awesome-github-stats/Models/UserStatsSvg.cs
@@ -56,7 +56,16 @@ namespace AwesomeGithubStats.Models
 
             return new MemoryStream(Encoding.UTF8.GetBytes(svgFinal));
         }
-
+        static string FormatNumber(int num)
+        {
+            if (num >= 100000)
+                return FormatNumber(num / 1000) + "K";
+            if (num >= 10000)
+            {
+                return (num / 1000D).ToString("0.#") + "K";
+            }
+            return num.ToString("#,0");
+        }
         private double CalculateCircleProgress(double value)
         {
             var radius = 50;
