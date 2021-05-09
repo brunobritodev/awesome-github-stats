6dc6c2852ea7a7ba873b62a64c2beea8c60a406d
Bruno Brito <bhdebrito@gmail.com>
2021-05-09 17:18:01 -0300

    <git-autocommit>

--- src/AwesomeGithubStats.Api/wwwroot/js/script.js
+++ src/AwesomeGithubStats.Api/wwwroot/js/script.js
@@ -148,8 +148,20 @@ let tooltip = {
     },
 };
 
+// Init a timeout variable to be used below
+let timeout = null;
 // refresh preview on interactions with the page
-document.addEventListener("blur", () => preview.update(), false);
+document.addEventListener('keyup', function (e) {
+    // Clear the timeout if it has already been set.
+    // This will prevent the previous task from executing
+    // if it has been less than <MILLISECONDS>
+    clearTimeout(timeout);
+
+    // Make a new timeout set to go off in 1000ms (1 second)
+    timeout = setTimeout(function () {
+        preview.update();
+    }, 1000);
+});
 document.addEventListener("click", () => preview.update(), false);
 
 // when the page loads
