785178982b4b03407368b322a44f8f63fca69f09
Bruno Brito <bhdebrito@gmail.com>
2021-05-09 01:29:51 -0300

    <git-autocommit>

--- src/AwesomeGithubStats.Core/Models/RankDegree.cs
+++ src/AwesomeGithubStats.Core/Models/RankDegree.cs
@@ -1,14 +1,10 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 
 namespace AwesomeGithubStats.Core.Models
 {
     public class RankDegree : Dictionary<string, int>
     {
         public double MaxPoints { get; set; } = 20000;
-        public int Total()
-        {
-            return this.Values.Sum();
-        }
+
     }
 }
