a0eeb3e4b2eed470bb687b87c68945626b6fa438
Bruno Brito <bhdebrito@gmail.com>
2021-05-09 10:48:13 -0300

    <git-autocommit>

--- src/AwesomeGithubStats.Core/Interfaces/ISvgService.cs
+++ src/AwesomeGithubStats.Core/Interfaces/ISvgService.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using AwesomeGithubStats.Core.Models;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace AwesomeGithubStats.Core.Interfaces
 {
     public interface ISvgService
     {
-                       
+        Task<Stream> GetUserStatsImage(UserRank rank);
     }
 }
--- src/AwesomeGithubStats.Core/Services/SvgService.cs
+++ src/AwesomeGithubStats.Core/Services/SvgService.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using AwesomeGithubStats.Core.Interfaces;
+using AwesomeGithubStats.Core.Models;
+using AwesomeGithubStats.Core.Models.Svgs;
+using Microsoft.Extensions.Options;
+using System.IO;
 using System.Threading.Tasks;
-using AwesomeGithubStats.Core.Interfaces;
 
 namespace AwesomeGithubStats.Core.Services
 {
@@ -11,13 +11,20 @@ namespace AwesomeGithubStats.Core.Services
     {
         private readonly string _contentRoot;
         private readonly ICacheService _cacheService;
+        private RankDegree _degree;
 
-        public SvgService(string contentRoot, ICacheService cacheService)
+        public SvgService(string contentRoot, ICacheService cacheService, IOptions<RankDegree> rankDegree)
         {
+            _degree = rankDegree.Value;
             _contentRoot = contentRoot;
             _cacheService = cacheService;
         }
 
-        
+        public Task<Stream> GetUserStatsImage(UserRank rank)
+        {
+            var svg = new UserStatsSvg(_contentRoot, _degree);
+
+            var content = await svg.Svg(rank, new Styles());
+        }
     }
 }
