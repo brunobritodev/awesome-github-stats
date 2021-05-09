b3003d293b687ac53e5a83a3cef36b8f237219a7
Bruno Brito <bhdebrito@gmail.com>
2021-05-09 01:45:03 -0300

    <git-autocommit>

--- src/AwesomeGithubStats.Core/Models/UserPoints.cs
+++ src/AwesomeGithubStats.Core/Models/UserPoints.cs
@@ -2,19 +2,19 @@
 {
     public class RankPoints
     {
-        public double PullRequests { get; set; } = 1;
-        public double Commits { get; set; } = 0.5;
-        public double CommitsToMyRepositories { get; set; } = 1;
-        public double CommitsToAnotherRepositories { get; set; } = 2;
-        public double PullRequestsToAnotherRepositories { get; set; } = 5;
-        public double Issues { get; set; } = 3;
-        public double CreatedRepositories { get; set; } = 1;
-        public double DirectStars { get; set; } = 2.0;
-        public double IndirectStars { get; set; } = 1;
-        public double ContributedTo { get; set; } = 1;
-        public double ContributedToOwnRepositories { get; set; } = 1;
+        public double Commits { get; set; } = 1;
         public double ContributedToNotOwnerRepositories { get; set; } = 10;
+        public double ContributedToOwnRepositories { get; set; } = 1;
+        public double CreatedRepositories { get; set; } = 1;
+        public double DirectStars { get; set; } = 3.5;
         public double Followers { get; set; } = 1;
+        public double IndirectStars { get; set; } = 1;
+        public double Issues { get; set; } = 1;
+        public double PullRequests { get; set; } = 1;
+        public double PullRequestsToAnotherRepositories { get; set; } = 5;
+        public double CommitsToMyRepositories { get; set; } = 1;
+        public double CommitsToAnotherRepositories { get; set; } = 10;
+        public double ContributedTo { get; set; } = 1;
 
         public double Total()
         {
