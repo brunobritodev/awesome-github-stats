9d5fe103c26c56c7e7e039193af538f8d317028a
Bruno Brito <bhdebrito@gmail.com>
2021-05-09 03:48:22 -0300

    <git-autocommit>

---
+++ src/awesome-github-stats/Models/Styles.cs
@@ -0,0 +1,10 @@
+﻿namespace AwesomeGithubStats.Models
+{
+    public class Styles
+    {
+        public string TextColor { get; set; }
+        public string TitleColor { get; set; }
+        public string IconColor { get; set; }
+        public bool ShowIcons { get; set; }
+    }
+}
