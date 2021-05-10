59858478c8e61052641078343361a96f13650433
Bruno Brito <bhdebrito@gmail.com>
2021-05-10 02:20:38 -0300

    format values & readme

--- src/AwesomeGithubStats.Core/Models/UserStats.cs
+++ src/AwesomeGithubStats.Core/Models/UserStats.cs
@@ -135,16 +135,23 @@ namespace AwesomeGithubStats.Core.Models
             return sumProducts; // / rankPoints.Total(Login);
         }
 
-
         static string FormatNumber(int num)
         {
             if (num >= 100000)
+            {
                 return FormatNumber(num / 1000) + "K";
+            }
             if (num >= 10000)
             {
                 return (num / 1000D).ToString("0.#") + "K";
             }
+            if (num >= 1000)
+            {
+                return (num / 1000D).ToString("0.#") + "K";
+            }
             return num.ToString("#,0");
+
+
         }
 
         public string TotalCommits() => FormatNumber(Commits);
