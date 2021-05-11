3f112fecf6a19768490e52174c681533bb7e21d5
Bruno Brito <bhdebrito@gmail.com>
2021-05-11 02:57:03 -0300

    brotli

--- src/AwesomeGithubStats.Api/Startup.cs
+++ src/AwesomeGithubStats.Api/Startup.cs
@@ -1,10 +1,14 @@
 using AwesomeGithubStats.Api.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using System.IO.Compression;
+using System.Linq;
+using System.Net.Mime;
 
 namespace AwesomeGithubStats.Api
 {
@@ -36,10 +40,21 @@ namespace AwesomeGithubStats.Api
             else
             {
                 services.AddMvcCore();
-                services.AddResponseCompression();
-                services.AddResponseCaching();
             }
 
+            services.AddResponseCaching();
+            services.AddResponseCompression(options =>
+            {
+                options.Providers.Add<BrotliCompressionProvider>();
+                options.Providers.Add<GzipCompressionProvider>();
+                options.EnableForHttps = true;
+                options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "image/jpeg", "image/png", "application/font-woff2", "image/svg+xml", MediaTypeNames.Application.Json });
+            });
+
+            services.Configure<BrotliCompressionProviderOptions>(options =>
+            {
+                options.Level = CompressionLevel.Optimal;
+            });
             services.ConfigureGithubServices(_configuration);
             services.AddHealthChecks();
 
@@ -48,6 +63,9 @@ namespace AwesomeGithubStats.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseResponseCompression();
+            app.UseResponseCaching();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -55,8 +73,7 @@ namespace AwesomeGithubStats.Api
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Awesome Github Stats v1"));
             }
 
-            app.UseResponseCaching();
-            app.UseResponseCompression();
+            app.UseHttpsRedirection();
             app.UseDefaultFiles(new DefaultFilesOptions() { DefaultFileNames = new[] { "index.html" } });
             app.UseStaticFiles();
             app.UseRouting();
